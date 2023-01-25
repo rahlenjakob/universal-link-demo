f55f7503c1ccc47a11431be140a7b19b5aebfb6a
Jakob Råhlén <jakob@qte.se>
2023-01-25 13:50:59 +0100

    initial handover cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const URL = "https://github.com/rahlenjakob"
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>App handovers</h1>
+        <p>Assumes that you have the Github app installed</p>
+        <section>
+          <h2>Direct link press</h2>
+          <a
+            href={URL}
+          >
+            Open
+          </a>
+        </section>
+        <section>
+          <h2>Button press with event handler</h2>
+          <button onClick={() => {
+            window.location.href = URL
+          }}>Open</button>
+        </section>
+        <section>
+          <h2>Button press with async event handler</h2>
+          <button onClick={async () => {
+            await new Promise(r => setTimeout(r, 2000));
+            window.location.href = URL
+          }}>Open</button>
+        </section>
+      </main>
     </div>
   );
 }
