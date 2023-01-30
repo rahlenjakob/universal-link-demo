340d3f7de6354243dac99d8a42cc8c551ae50010
Jakob Råhlén <jakob@qte.se>
2023-01-30 10:01:42 +0100

    made deeplink the default

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const URLToOpen = "https://app.bankid.com/?autostarttoken=faketoken&redirect=https://google.com"
+// export const URLToOpen = "https://app.bankid.com/?autostarttoken=faketoken&redirect=https://google.com"
+export const URLToOpen = "bankid:///"
